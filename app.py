d27ad5187296bceeddd2413477aa3f07819fa7bc
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2025-04-07 17:03:32 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def principal():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
