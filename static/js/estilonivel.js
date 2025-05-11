3308877a6fcddee0910a7b1e17e319aeb3256c2e
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2025-05-11 16:56:01 -0600

    actualizar js

--- static/js/estilonivel.js
+++ static/js/estilonivel.js
@@ -2,16 +2,15 @@
 const concepto = document.getElementById('concepto');
 const logro1 = document.getElementById('logro1');
 const labellogro = document.getElementById('labellogro');
-const nivel5 = document.getElementById('nivel5')
-const label5 = document.getElementById('label5')
-const nivel4 = document.getElementById('nivel4')
-const label4 = document.getElementById('label4')
-const nivel3 = document.getElementById('nivel3')
-const label3 = document.getElementById('label3')
-const nivel2 = document.getElementById('nivel2')
-const label2 = document.getElementById('label2')
-const leyenda1 = document.getElementById('leyenda1')
-
+const nivel5 = document.getElementById('nivel5');
+const label5 = document.getElementById('label5');
+const nivel4 = document.getElementById('nivel4');
+const label4 = document.getElementById('label4');
+const nivel3 = document.getElementById('nivel3');
+const label3 = document.getElementById('label3');
+const nivel2 = document.getElementById('nivel2');
+const label2 = document.getElementById('label2');
+const leyenda1 = document.getElementById('leyenda1');
 
 concepto.addEventListener('change', function () {
     if (concepto.checked) {
@@ -45,15 +44,15 @@ concepto.addEventListener('change', function () {
 const proceso = document.getElementById('proceso');
 const logro2 = document.getElementById('logro2');
 const labellogro2 = document.getElementById('labellogro2');
-const nivel55 = document.getElementById('nivel5')
-const label55 = document.getElementById('label55')
-const nivel44 = document.getElementById('nivel44')
-const label44 = document.getElementById('label44')
-const nivel33 = document.getElementById('nivel33')
-const label33 = document.getElementById('label33')
-const nivel22 = document.getElementById('nivel22')
-const label22 = document.getElementById('label22')
-const leyenda2 = document.getElementById('leyenda2')
+const nivel55 = document.getElementById('nivel55');
+const label55 = document.getElementById('label55');
+const nivel44 = document.getElementById('nivel44');
+const label44 = document.getElementById('label44');
+const nivel33 = document.getElementById('nivel33');
+const label33 = document.getElementById('label33');
+const nivel22 = document.getElementById('nivel22');
+const label22 = document.getElementById('label22');
+const leyenda2 = document.getElementById('leyenda2');
 
 proceso.addEventListener('change', function () {
     if (proceso.checked) {
@@ -87,15 +86,15 @@ proceso.addEventListener('change', function () {
 const actitud = document.getElementById('actitud');
 const logro3 = document.getElementById('logro3');
 const labellogro3 = document.getElementById('labellogro3');
-const nivel555 = document.getElementById('nivel555')
-const label555 = document.getElementById('label555')
-const nivel444 = document.getElementById('nivel444')
-const label444 = document.getElementById('label444')
-const nivel333 = document.getElementById('nivel333')
-const label333 = document.getElementById('label333')
-const nivel222 = document.getElementById('nivel222')
-const label222 = document.getElementById('label222')
-const leyenda3 = document.getElementById('leyenda3')
+const nivel555 = document.getElementById('nivel555');
+const label555 = document.getElementById('label555');
+const nivel444 = document.getElementById('nivel444');
+const label444 = document.getElementById('label444');
+const nivel333 = document.getElementById('nivel333');
+const label333 = document.getElementById('label333');
+const nivel222 = document.getElementById('nivel222');
+const label222 = document.getElementById('label222');
+const leyenda3 = document.getElementById('leyenda3');
 
 actitud.addEventListener('change', function () {
     if (actitud.checked) {
