5e1f4ffaf533f976098196c7fb55f542085155f3
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2025-05-15 16:11:11 -0600

    supabase

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template  # type: ignore
+from flask_sqlalchemy  import SQLAlchemy
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
+
+app.config("SQLALCHEMY_DATABASE_URI") = "postgresql://postgres:@db.mhkcnviednvvvosahgoa.supabase.co:5432/postgres"
+
+db.init_app(app)
+
+class SupaUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def inicio():
