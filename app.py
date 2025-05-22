326f5ba7d91827d573a357dbbadde0d818816946
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2025-05-22 13:25:44 -0600

    login

--- app.py
+++ app.py
@@ -5,49 +5,52 @@ import os
 
 load_dotenv()
 
+
+
+app = Flask(__name__)
+
+app.secret_key = os.getenv('FLASK_SECRET_KEY', 'supersecretkey')
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
 
 supabase: Client = create_client(url, key)
 
-app = Flask(__name__)
-app.secret_key = 'tu_clave_secreta'
-
-@app.route('/registrar', methods=['GET','POST'])
-def registrar():
-    if request.method == 'POST':
-        data = request.get_json()
-        email = request.form['email']
-        password = request.form['password']
-        nombre = request.form['nombre']
-        
-        #Registrar new user
-        try:
-            respuesta = supabase.from_('login').insert(data).execute()
-            
-            if respuesta.data:
-                flash('Datos insertados correctamente','success')
-                return redirect(url_for('inicio'))
-            else:
-                flash('Error al insetar los datos.', 'error')
-                return redirect(url_for('login'))
-        except Exception as e:
-            return redirect(url_for('registrar'))
 
 @app.route('/')
-def login():
-    return render_template('login1.html')
+def home():
+    if 'user_email' in session:
+        return render_template('inicio.html', email=session['user_email'])
+    return redirect(url_for('login'))
 
 @app.route('/inicio')
 def inicio():
     return render_template('/inicio.html')
-""" if 'user' in session:
-        return render_template('/inicio.html', user=session['user'])
-    else:
-        return redirect(url_for('login'))
-"""
-    
-        
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        print(password)
+        try:
+            user = supabase.auth.sign_in_with_password({
+                "email": email,
+                "password": password
+            })
+            session['user_email']=email
+            session['access_token'] = user.session.access_token
+            flash('Inicio de sesion exitoso.','success')
+            return redirect(url_for('home'))
+        except Exception as e:
+            flash(f'Error en el inicio de sesion: {str(e)}','danger')
+    return render_template('login1.html')  
+
+@app.route('/logout')
+def logout():
+    session.pop('user_email', None)
+    session.pop('access_token', None)
+    flash('Has cerrado sesion correctamente.', 'info')
+    return redirect(url_for('login'))
 
 @app.route('/registro')
 def registro():
@@ -76,13 +79,6 @@ def rubrica():
 @app.route("/grupo")
 def grupo():
     return render_template("/vistas/grupo.html")
-
-@app.route('/logout')
-def logout():
-    session.clear()
-    flash('Has cerrado sesión', 'info')
-    return redirect(url_for('login'))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
