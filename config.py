a89863a97ed7a62d507901b722bf743d01dbdf20
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2025-05-27 21:36:19 -0600

    area

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ class Config:
     SUPABASE_URL = os.getenv('SUPABASE_URL')
     SUPABASE_KEY = os.getenv('SUPABASE_KEY')
     SECRET_KEY = os.getenv('SECRET_KEY') or 'una-clave-secreta-muy-segura'
+     
